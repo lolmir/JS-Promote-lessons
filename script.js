8c9ccadc932f357931e714711109a33e6fcbbb09
lolixa <lolixa9002@gmail.com>
2022-10-04 18:44:09 +0300

    5HW

--- script.js
+++ script.js
@@ -2,7 +2,6 @@
 
 const URL = "https://raw.githubusercontent.com/GeekBrainsTutorial/online-store-api/master/responses"
 const GOODS = "/catalogData.json"
-
 const url = `${URL}${GOODS}`;
 
 function service(url) {
@@ -17,54 +16,36 @@ function service(url) {
 	})
 }
 
-const goods = [
-	{ product_name: 'Shirt', price: 150 },
-	{ product_name: 'Socks', price: 50 },
-	{ product_name: 'Jacket', price: 350 },
-	{ product_name: 'Shoes', price: 250 },
-];
-
-class GoodsItem {
-	constructor({ product_name, price }) {
-		this.product_name = product_name;
-		this.price = price;
-	}
-	render() {
-		return `
-		<div class="goods-item">
-		<h3>${this.product_name}</h3>
-		<p>${this.price}</p>
-	</div>`;
-	}
-}
-class GoodsList {
-	items = [];
-	fetchGoods() {
-		return new Promise((resolve) => {
-			service(url).then((data) => {
-				this.items = data;
-				resolve()
+const app = new Vue({
+	el: '#root',
+	data: {
+		goods: [],
+		search: '',
+		isVisibleCart: false
+	},
+	mounted() {
+		service(url).then((data) => {
+			this.goods = data;
+		});
+	},
+	computed: {
+		calculateAllPrice() {
+			return this.goods.reduce((prev, goods) => {
+				return prev + goods.price;
+			}, 0)
+		},
+		filterGoods() {
+			return this.goods.filter((item) => {
+				const regExp = new RegExp(this.search);
+				return regExp.test(item.product_name)
 			})
-		})
-	};
+		},
+	},
+	methods: {
+		setVisibleCard() {
+			this.isVisibleCart = !this.isVisibleCart;
+		}
+	},
 
-	calculateAllPrice() {
-		return this.items.reduce((prev, item) => {
-			return prev + item.price;
-		}, 0)
-	}
-	render() {
-		const goods = this.items.map(item => {
-			const goodItem = new GoodsItem(item);
-			return goodItem.render()
-		}).join('');
+})
 
-		document.querySelector('.goods-list').innerHTML = goods;
-	}
-}
-
-const goodsList = new GoodsList();
-goodsList.fetchGoods().then(() => {
-	goodsList.render();
-});
-goodsList.calculateAllPrice();
